f0882b6e045f486b38358a80bcacfedefa2f47a8
Favian M Rasyad R <rasyad101112@gmail.com>
2025-09-10 11:38:43 +0700

    summary

---
+++ main/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+import uuid
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('transfer', 'Transfer'),
+        ('update', 'Update'),
+        ('exclusive', 'Exclusive'),
+        ('match', 'Match'),
+        ('rumor', 'Rumor'),
+        ('analysis', 'Analysis'),
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    thumbnail = models.URLField(blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
+    price = models.IntegerField()
+    category = models.CharField()
+    
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_news_hot(self):
+        return self.news_views > 20
+        
+    def increment_views(self):
+        self.news_views += 1
+        self.save()
---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406495634',
+        'name': 'Favian Muhammad Rasyad Reswara',
+        'class': 'PBP B',
+        'nama_toko': 'fabrizio romano shop'
+    }
+
+    return render(request, "main.html", context)
