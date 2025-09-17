da87419d5c600e2516fe10db2e56c77cfd60015c
Favian M Rasyad R <rasyad101112@gmail.com>
2025-09-17 16:16:37 +0700

    tugas 4

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 import uuid
@@ -22,6 +23,7 @@ class Product(models.Model):
     is_featured = models.BooleanField(default=False)
     price = models.IntegerField(null=True, blank=True, default=0)
     category = models.CharField(choices=CATEGORY_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return self.title
