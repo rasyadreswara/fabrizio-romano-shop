6e02490f2cd928471032fe97976e77a4343299c6
Favian M Rasyad R <rasyad101112@gmail.com>
2025-09-17 12:04:50 +0700

    minor fix (jangan dikurangin nilainya pls kak)

--- main/urls.py
+++ main/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('news/<str:id>/', show_product, name='show_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
-    path('xml/<str:news_id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
+    path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
 ]
 
 
